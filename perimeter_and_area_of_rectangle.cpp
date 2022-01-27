6890cd3dd90fd5caf42dca900140f5e3e947746c
Shashank Sheshagiri <shashankseshagiri@gmail.com>
2022-01-27 19:12:24 +0530

    perimeter_and_area_of_rectangle.cpp

---
+++ perimeter_and_area_of_rectangle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int height=7,wigth=5 ,per;
+    per=2*( height+wigth );
+    printf("Perimeter of the rectangle : %d\n ", per);
+    printf("area of the rectangle : %d \n ",height*wigth);
+    return 0;
+}
